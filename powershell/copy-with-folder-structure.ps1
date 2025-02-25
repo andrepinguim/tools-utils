f6f904436f76ab8717cb3eb16be09db32a431fda
André Vazquez <andrepinguim@users.noreply.github.com>
2025-02-25 14:38:29 -0300

    Create copy-with-folder-structure.ps1

---
+++ powershell/copy-with-folder-structure.ps1
@@ -0,0 +1,19 @@
+﻿$SourceFolder = "D:\Downloads"
+$DestinationFolder = "D:\test"
+$IncludeFiles = ("*.jpeg","*.jpg")
+
+Get-ChildItem $SourceFolder -Recurse -Include $IncludeFiles | Where-Object {$_.LastWriteTime -gt $ChangesStarted} | ForEach-Object {
+    $PathArray = $_.FullName.Replace($SourceFolder,"").ToString().Split('\') 
+
+    $Folder = $DestinationFolder
+
+    for ($i=1; $i -lt $PathArray.length-1; $i++) {
+        $Folder += "\" + $PathArray[$i]
+        if (!(Test-Path $Folder)) {
+            New-Item -ItemType directory -Path $Folder
+        }
+    }   
+    $NewPath = Join-Path $DestinationFolder $_.FullName.Replace($SourceFolder,"")
+
+    Copy-Item $_.FullName -Destination $NewPath  
+}
